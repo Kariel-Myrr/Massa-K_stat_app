61aca55023098c011ddf01cd0e4186d059c02419
SaZha <kaa5843771@yandex.ru>
2019-07-22 19:18:42 +0300

    При запуске программы включается bluetooth, при выходе из программы он отключается

--- app/src/main/java/com/example/lesson_1/massa_k_app/MainActivity.kt
+++ app/src/main/java/com/example/lesson_1/massa_k_app/MainActivity.kt
@@ -6,19 +6,29 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    /** Создаём объект java класса, через который будет проводиться включение bluetooth
+    Передаём параметром текущую activity, чтобы можно было выовдить сообщения
+    при включении, отключении bluetooth или в случае ошибок
+    Объект сделан неизменяемым, чтобы избежать случайных ошибок*/
+    val bluetoothInstruments = BluetoothInstruments(this)
+
+    /**TODO
+     *  Разобраться с тем, за что отвечает параметр z и нужен ли он вообще
+     */
     fun getParametres(): MutableList<Float> { // CMD_GET_MASSA
-        var m : Float = 0.toFloat()
-        var t : Float = 0.toFloat()
-        var z : Float = 0.toFloat()
+        var massNetto : Float = 0F  // Масса НЕТТО (масса продукта минус масса тары)
+        var massTara : Float = 0F   // Масса пустой тары
+        var z : Float = 0F          // ???
 
         // CMD_ACK_MASSA    success
         // CMD_ERROR        error
-        var parametres : MutableList<Float> = arrayListOf()
-        parametres.add(0, m)
-        parametres.add(1,t)
-        parametres.add(2,z)
 
-        return parametres
+        var currentParametres : MutableList<Float> = arrayListOf() // Текущие параметры устройства
+        currentParametres.add(0, massNetto)
+        currentParametres.add(1, massTara)
+        currentParametres.add(2, z)
+
+        return currentParametres
     }
 
     fun setTare(t: Float): String { //CMD_SET_TARE
@@ -55,14 +65,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bluetoothInstruments.enableBT()     // Включаем bluetooth при запуске программы
 
         var Mass : Float
         var Tare : Float
         var Zero : Float
-        var par = getParametres()
-        Mass = par[0]
-        Tare = par[1]
-        Zero = par[2]
+        var currentParameters = getParametres()
+        Mass = currentParameters[0]
+        Tare = currentParameters[1]
+        Zero = currentParameters[2]
 
         renew.setOnClickListener {
             setUIparametres()
@@ -88,5 +99,12 @@ class MainActivity : AppCompatActivity() {
 
             setUIparametres()
         }
+    }
+
+    // При закрытии программы сначала отключаем bluetooth, потом закрываем все широковещательные каналы
+    override fun onDestroy() {
+        bluetoothInstruments.disableBT()    // Отключаем bluetooth
+        bluetoothInstruments.finish()       // Закрываем широковещательные каналы
+        super.onDestroy()
     }
 }
