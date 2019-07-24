fa9ce39fb6d8e87d39777c5c58ae8e4245eec5b7
Kira <kirill.kreslavski@gmail.com>
2019-07-24 13:52:36 +0300

    new concept basic maquette

--- app/src/main/java/com/example/lesson_1/massa_k_app/activity_history_main.kt
+++ app/src/main/java/com/example/lesson_1/massa_k_app/activity_history_main.kt
@@ -3,10 +3,10 @@ package com.example.lesson_1.massa_k_app
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class activity_history_main : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.weight_main)
+        setContentView(R.layout.activity_history_main)
     }
 }
