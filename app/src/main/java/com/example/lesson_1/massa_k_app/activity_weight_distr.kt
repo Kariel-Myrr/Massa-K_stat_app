4219094ca0c86ed55ec27f45abec3c46b0e702e5
Kira <kirill.kreslavski@gmail.com>
2019-07-24 15:57:18 +0300

    ui of weight distribution added

---
+++ app/src/main/java/com/example/lesson_1/massa_k_app/activity_weight_distr.kt
@@ -0,0 +1,12 @@
+package com.example.lesson_1.massa_k_app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_weight_distr : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weight_distr)
+    }
+}
