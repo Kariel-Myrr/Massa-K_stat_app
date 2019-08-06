64416bd72e2d077f4cdb068fae684c5980096490
KreslavskiKD <kirill.kreslavski@gmail.com>
2019-08-06 15:27:55 +0300

    activity changing fixed

--- app/src/main/java/com/example/lesson_1/massa_k_app/FragmentWeightMain.kt
+++ app/src/main/java/com/example/lesson_1/massa_k_app/FragmentWeightMain.kt
@@ -1,14 +1,27 @@
 package com.example.lesson_1.massa_k_app
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import kotlinx.android.synthetic.main.fragment_weight_main.*
 
 class FragmentWeightMain : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_weight_main, null)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        weight.setOnClickListener {
+            val intent = Intent(activity, activity_weight_second::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
